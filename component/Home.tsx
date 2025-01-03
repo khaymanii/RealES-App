fdecd168abd828fd45725b983c46c1a5f48a3ee6
khaymanii <kennethakpo61@gmail.com>
2025-01-04 00:14:04 +0100

    Enhance HomeCard component with image display and data integration

--- component/Home.tsx
+++ component/Home.tsx
@@ -5,13 +5,11 @@ import HomeCard from "./HomeCard";
 
 export default function Home() {
   return (
-    <View style={tw`mx-4 mb-8`}>
+    <View style={tw` mb-8`}>
       <Text style={tw`font-semibold text-xl text-center py-8`}>
         Homes For You
       </Text>
-      <View>
-        <HomeCard />
-      </View>
+      <HomeCard />
     </View>
   );
 }
